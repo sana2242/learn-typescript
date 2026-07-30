989717940c0262789ddc1f4c0980e8a6951e3a79
San <sa7476183@gmail.com>
2026-07-30 16:00:16 +0300

    Add objects lesson

---
+++ 02-Object/dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let personExOne;
+personExOne = {
+    name: "Alice",
+    age: 30,
+    jobTitle: "Engineer",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+let personExTwo = {
+    name: "Tom",
+    age: 31,
+    jobTitle: "Doctor",
+    address: {
+        street: "123 Main St",
+        city: "Wonderland",
+    },
+};
+//# sourceMappingURL=index.js.map
---
+++ 02-Object/src/index.ts
@@ -0,0 +1,41 @@
+let personExOne: {
+    name: string;
+    age: number;
+    jobTitle?: string;
+    address: {
+        street: string;
+        city: string;
+    };
+};
+
+
+personExOne = {
+name: "Alice",
+age: 30,
+jobTitle: "Engineer",
+address: {
+    street: "123 Main St",
+    city:"Wonderland",
+
+},
+};
+
+
+let personExTwo: {
+    name: string;
+    age: number;
+    jobTitle?: string;
+    address: {
+        street: string;
+        city: string;
+    };
+} = {
+name: "Tom",
+age: 31,
+jobTitle: "Doctor",
+address: {
+    street: "123 Main St",
+    city:"Wonderland",
+
+},
+};
